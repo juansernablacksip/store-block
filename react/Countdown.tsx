1ceab3ec5b41d6cb890d530933dde3f6eb113570
juansernablacksip <juanserna@blacksip.com>
2020-11-11 12:22:10 -0500

    Add new component

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -2,19 +2,18 @@ import React, { useState } from 'react'
 import { TimeSplit } from './typings/global'
 import { tick, getTwoDaysFromNow } from './utils/time'
 import { useCssHandles } from "vtex.css-handles"
-import { FormattedMessage } from 'react-intl'
+//-import { FormattedMessage } from 'react-intl'
 
 interface CountdownProps {
-   title: string
    targetDate: string
+   //,title: string
 }
 
 const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
-
+// const CSS_HANDLES = ['container', 'countdown', 'title']
 const CSS_HANDLES = ["countdown"]
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  title,
   targetDate = DEFAULT_TARGET_DATE,
 }) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
@@ -23,17 +22,26 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
     seconds: '00',
   })
 
-  const titleText = title || <FormattedMessage id="countdown.title" />
+  //const titleText = title || <FormattedMessage id="countdown.title" />
   const handles = useCssHandles(CSS_HANDLES)
 
   tick(targetDate, setTime)
 
-  return (
-    <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
-      <div className={`${handles.title} db tc`}>{titleText}</div>
+  /*
+    return (
+-   <div className={`${handles.container} t-heading-2 fw3 w-100 pt7 pb6 c-muted-1 db tc`}>
+-     <div className={`${handles.title} db tc`}>
+-       { titleText }
+-     </div>
       <div className={`${handles.countdown} db tc`}>
         {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
       </div>
+-   </div>
+  )
+  */
+  return (
+    <div className={`${handles.countdown} db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
     </div>
   )
 }
@@ -43,11 +51,11 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
-   title: {
+   /*title: {
      title: 'I am a title',
      type: 'string',
      default: null,
-   },
+   },*/
     targetDate: {
       title: 'Final date',
       description: 'Final date used in the countdown',

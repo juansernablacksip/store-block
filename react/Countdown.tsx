02beb4d11d28b66f28ab7c0233d4dd83451cd1d2
juansernablacksip <juanserna@blacksip.com>
2020-11-30 15:11:09 -0500

    Custom store Part 1

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import styles from './Countdown.css'
 
 interface CountdownProps { }
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+  return (
+  <div className={styles.myDivGeneral}>
+    <div className={styles.myDiv1}>
+      <h1 className={styles.myTextLeft}> 
+        Envío gratis desde S/.250
+      </h1>
+      <a href="http://www.google.com" className={styles.myTextRight}>
+        Ver condiciones
+      </a>
+    </div>
+    <div className={styles.myDiv2}>
+      <h1 className={styles.myTextTHNLeft}>
+        Visita también:
+      </h1>
+      <a href="http://www.google.com">
+        <img src="https://www.triathlon.com.pe/media/wysiwyg/THN_BLANCO.png" alt="THN Catalogo" width="50" height="30"/>
+      </a>
+      <h2 className={styles.myTextTHNRight}>
+        CATÁLOGO
+      </h2>
+    </div>
+  </div>
+  )
 }
 
 Countdown.schema = {
